1f873990ec7696d78324e1c3d0dc7e50ebe8cac5
Gabriel Hare <ghare@reification.io>
2021-02-16 21:09:43 -0800

    Add toggle control scripts

---
+++ Unity_Importer/Assets/Reification/AutoImport/Scripts/KeyToggleEnabled.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Reification {
+	public class KeyToggleEnabled: MonoBehaviour {
+		/// <summary>
+		/// Behaviors controlled by toggle key
+		/// </summary>
+		/// <remarks>
+		/// Behaviors enabled states are cycled independently
+		/// relative to their current states.
+		/// </remarks>
+		public List<Behaviour> targetList;
+
+		/// <summary>
+		/// Key pressed to cycle enabled state
+		/// </summary>
+		public KeyCode toggle = KeyCode.None;
+
+		/// <summary>
+		/// Optional key that must be held to cycle enabled state
+		/// </summary>
+		public KeyCode safety = KeyCode.None;
+
+		void Update() {
+			if(safety != KeyCode.None && !Input.GetKey(safety)) return;
+			if(!Input.GetKeyDown(toggle)) return;
+			foreach(var target in targetList) target.enabled = !target.enabled;
+		}
+	}
+}
