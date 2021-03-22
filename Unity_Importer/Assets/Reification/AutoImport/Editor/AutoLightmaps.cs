78d2025f87867431001be425f92fd5252ea4499a
Gabriel Hare <ghare@reification.io>
2021-03-21 23:31:54 -0700

    AutoLightmap bakes multiple scenes

--- Unity_Importer/Assets/Reification/AutoImport/Editor/AutoLightmaps.cs
+++ Unity_Importer/Assets/Reification/AutoImport/Editor/AutoLightmaps.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright 2021 Reification Incorporated
 // Licensed under Apache 2.0. All Rights reserved.
 
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -14,8 +15,7 @@ namespace Reification {
 
     [MenuItem(menuItemName, validate = true, priority = menuItemPriority)]
     static private bool Validate() {
-      if(Selection.objects.Length == 0) return false;
-      foreach(var scene in Selection.objects) if(null == scene as SceneAsset) return false;
+      foreach(var sceneAsset in Selection.objects) if(null == sceneAsset as SceneAsset) return false;
       return true;
     }
 
@@ -23,47 +23,73 @@ namespace Reification {
     static private void Execute() {
       Undo.IncrementCurrentGroup();
       Undo.SetCurrentGroupName("Bake Lightmaps");
-      foreach(var scene in Selection.objects) ApplyTo(AssetDatabase.GetAssetPath(scene));
 
-      // NOTE: BakeMultipleScenes loads and bakes scenes simultaneously
-      // so that each scene can contribute to lightmaps in adjacent scenes
-      // Here, the scenes are opened successively for configuration and baking.
-      // OPTION: Separate interface for enqueued baking? This might be helpful since
-      // it could apply without overriding lightmaps.
+      // If no scenes are selected configure and bake combined lightmap for all open scenes
+      var scenePathList = new List<string>();
+      if(Selection.objects.Length > 0) {
+        foreach(var sceneAsset in Selection.objects) scenePathList.Add(AssetDatabase.GetAssetPath(sceneAsset));
+      } else {
+        var sceneSetupList = EditorSceneManager.GetSceneManagerSetup();
+        foreach(var sceneSetup in sceneSetupList) scenePathList.Add(sceneSetup.path);
+      }
+
+      ApplyTo(scenePathList.ToArray());
     }
 
-    static public void ApplyTo(string scenePath) {
+    static public void ApplyTo(params string[] scenePathList) {
+      if(scenePathList.Length == 0) return;
+
+
+      // Open and apply a uniform configuration to each scene
+      // QUESTION: Is it necessary to open each scene in single mode - maybe active scene could be changed instead?
+      EditorSceneManager.SaveOpenScenes();
       var sceneSetup = EditorSceneManager.GetSceneManagerSetup();
       try {
-        // IMPORTANT: Lightmapping.Bake() should be called when only scenes contributing to bake are open
-        var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
-        var giWorkflowMode = Lightmapping.giWorkflowMode;
-        Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
-        // IMPORTANT: Bake() should not be called when giWorkflowMode == Iterative
-
-        ConfigureLightmaps(scene);
-
-        // Unity 2019.4
-        // PROBLEM: If AssetDatabase.StartAssetEditing() has been called lightmap generation will fail and loop with error:
-        // "Cannot call SetTextureImporterSettings(paths[], settings[]) after StartAssetImporting()"
-        // Which is accompanied by errors:
-        // "Could not access TextureImporter at path"
-        // "Could not read textures from Assets"
-        // "Integrate failed on Write Lighting Data job"
-        // PROBLEM: Calling AssetDatabase.StopAssetEditing() during import puts editor into hung state.
-        // SOLUTION: Enqueue scenes to be baked and wait for importing to complete.
-        // CAUTION: This callback might not work in batch mode!
-        // https://forum.unity.com/threads/editorapplication-update-callback-is-not-called-while-run-build-from-command-line.512380/
-        Lightmapping.Bake();
-        Lightmapping.giWorkflowMode = giWorkflowMode;
-        EditorSceneManager.SaveScene(scene);
+        foreach(var scenePath in scenePathList) {
+          var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+          EditorSceneManager.SetActiveScene(scene);
+          ConfigureLightmaps(scene);
+          EditorSceneManager.SaveScene(scene);
+        }
       } finally {
+        // IMPORTANT: RestoreSceneManagerSetup must be called before Bake() or BakeMultipleScenes()
+        // since it will halt that process.
+        EditorSceneManager.RestoreSceneManagerSetup(sceneSetup);
+      }
+
+      // Unity 2019.4
+      // PROBLEM: If AssetDatabase.StartAssetEditing() has been called lightmap generation will fail and loop with error:
+      // "Cannot call SetTextureImporterSettings(paths[], settings[]) after StartAssetImporting()"
+      // Which is accompanied by errors:
+      // "Could not access TextureImporter at path"
+      // "Could not read textures from Assets"
+      // "Integrate failed on Write Lighting Data job"
+      // PROBLEM: Calling AssetDatabase.StopAssetEditing() during import puts editor into hung state.
+      // SOLUTION: Enqueue scenes to be baked and wait for importing to complete.
+
+      // PROBLEM: BakeMultipleScenes is asynchronous and can be interrupted
+      // SOLUTION: Open all scenes and call Bake()
+      // NOTE: Baked lighting, including RTGI, will be referenced between scenes
+      // WARNING: Light probe data is not partitioned between scenes
+      // https://docs.unity3d.com/2019.4/Documentation/ScriptReference/Lightmapping.BakeMultipleScenes.html
+
+      // IMPORTANT: Bake() should not be called when giWorkflowMode == Iterative
+      var giWorkflowMode = Lightmapping.giWorkflowMode;
+      try {
+        Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
+
+        // IMPORTANT: Lightmapping.Bake() should be called when only scenes contributing to bake are open
+        for(var s = 0; s < scenePathList.Length; ++s) EditorSceneManager.OpenScene(scenePathList[s], s == 0 ? OpenSceneMode.Single : OpenSceneMode.Additive);
+        Lightmapping.Bake();
+        EditorSceneManager.SaveOpenScenes();
+      } finally {
+        Lightmapping.giWorkflowMode = giWorkflowMode;
         EditorSceneManager.RestoreSceneManagerSetup(sceneSetup);
       }
     }
 
     // QUESTION: Should lower levels of detail have different lightmap settings?
-    // OPTION: Terrain scale 0 - see hover pop-up, or is non-static with mesh marked as contributing - only
+    // OPTION: Terrain scale=0 (see hover pop-up), or is non-static with mesh marked as contributing-only
     // OPTION: Scale relative to object size, and keep lower levels of detail increased?
 
     // Expose settings in the Lighting panel to scripted modification 
@@ -72,6 +98,7 @@ namespace Reification {
         EditorSceneManager.SaveOpenScenes();
         EditorSceneManager.LoadScene(scene.path, LoadSceneMode.Single);
       }
+      EditorSceneManager.SetActiveScene(scene);
 
       // https://forum.unity.com/threads/access-lighting-window-properties-in-script.328342/#post-2669345
       // GetLightmapSettings() is declared as a static accessor in LightmapEditorSettings
